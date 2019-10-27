95c7268514cbd8d550f18a448c8247515040f1c8
yang.wei <1042744766@qq.com>
2019-10-27 12:11:44 +0800

    实现代码

---
+++ TaiChi.Framework/TaiChi.Core.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaiChi.Core.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TaiChi.Framework/TaiChi.Framework.CoreConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace TaiChi.Framework.CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var user = new { 
+                Id=11,
+                Name="HelloDavy",
+                Age=18
+            };
+
+            Console.WriteLine(user);
+            var options = new JsonSerializerOptions();
+            options.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(UnicodeRanges.All);
+            var json = JsonSerializer.Serialize(user,options);
+            Console.WriteLine(json);
+
+            Console.WriteLine("**************************************");
+            {
+                SharpSix six = new SharpSix();
+                People people = new People()
+                {
+                    Id = 505,
+                    Name = "马尔凯蒂"
+                };
+                six.Show(people);
+            }
+
+            Console.WriteLine("**************************************");
+            {
+                SharpSeven seven = new SharpSeven();
+                seven.Show();
+            }
+
+        }
+    }
+}
