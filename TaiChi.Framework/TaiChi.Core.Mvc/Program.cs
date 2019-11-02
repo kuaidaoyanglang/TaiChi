30e88ab54453074d4739dc7478eda3f097ab8f3f
yang.wei <1042744766@qq.com>
2019-11-02 23:15:32 +0800

    添加注释

--- TaiChi.Framework/TaiChi.Core.Mvc/Program.cs
+++ TaiChi.Framework/TaiChi.Core.Mvc/Program.cs
@@ -16,6 +16,18 @@ namespace TaiChi.Core.Mvc
         ///     寄宿在IIS上，IIS监听端口，做转发，有.net framewrok站点做业务逻辑处理，响应请求。
         /// 
         /// core 实际上是一个控制台程序,程序从main进入。
+        /// 
+        /// 
+        /// 部署：
+        ///     确保运行时正确安装；
+        ///     选择发布项目，发布到指定文件夹；
+        ///     新建站点；
+        ///     程序池里选择非托管代吗；
+        ///     
+        /// appsetting.json对应于Web.config;默认采用json格式存储；
+        /// asp.net:ConfigurationManager
+        /// asp.net core:依赖于IConfiguration
+        /// 
         /// </summary>
         /// <param name="args"></param>
         public static void Main(string[] args)
